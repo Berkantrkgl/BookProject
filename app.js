e7cd116604e1d21fb395f3d5c818652ef1a02a05
Berkan Turkoglu <berkan.trkgl35@gmail.com>
2023-12-07 21:51:18 +0300

    Finish most of the web design

--- app.js
+++ app.js
@@ -14,8 +14,20 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+// Read Notes page
+app.get("/note-detail", (req, res) => {
+    res.render("note-detail.ejs");
+});
 
+// Take Notes page
+app.get("/take-note", (req, res) => {
+    res.render("take-note.ejs");
+});
 
+// Rate book
+app.get("/rate", (req, res) => {
+    res.render("rate.ejs");
+});
 
 // Running server 
 app.listen(port, () => {
