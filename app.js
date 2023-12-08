bec07c9170fe7ab2e46adfe4d43e416d427baac8
Berkan Turkoglu <berkan.trkgl35@gmail.com>
2023-12-08 15:08:27 +0300

    Edit app.js file

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
 
 // Configuration static folder
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
+const API_URL = "http://covers.openlibrary.org/api";
 
 // For the query for API http://covers.openlibrary.org/api/query
 
@@ -38,7 +43,25 @@ app.get("/delete", (req, res) => {
 
 // Search note 
 app.get("/search", (req, res) => {
+
+})
+
+// Add new note
+app.post("/addNote", async (req, res) => {
+    const bookName = req.body.bookName;
+    const isbn = req.body.isbn;
+    const rating = req.body.rating;
+    const notes = req.body.notes;
+
+    try {
+        const response = await axios.get("http://covers.openlibary.org/api/query?isbn=9780385533225");
+        const result = response.data;
+        console.log(result);
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+    }
     
+
 })
 
 // Running server 
